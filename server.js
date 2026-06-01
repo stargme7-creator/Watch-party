d4bb266811601e97a3618ca13f7b126001da5682
stargme7-creator <stargme7@gmail.com>
2026-06-01 15:29:28 +0530

    Simplify OPTIONS method handling in CORS headers

--- server.js
+++ server.js
@@ -23,9 +23,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
+    if (req.method === 'OPTIONS') return res.sendStatus(200);
     next();
 });
 
