c2856a097887c4f5bf437ad2ec787354a0afc8e4
stargme7-creator <stargme7@gmail.com>
2026-06-04 09:18:25 +0530

    Update nodemailer transport configuration

--- server.js
+++ server.js
@@ -26,10 +26,15 @@ const pool = new Pool({
 });
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
